4f88340ec5ab296d6c7331f480788fa377476eea
Florian Breuer <f.breuer@a3plus.de>
2016-04-11 13:48:28 +0200

    test
    Test

---
+++ src/AppBundle/Controller/GenusController.php
@@ -0,0 +1,25 @@
+<?php
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class GenusController extends Controller
+{
+    /**
+     * @Route("/genus/{genusName}")
+     */
+    public function showAction($genusName)
+    {
+        $notes = [
+            'Octopus asked me a riddle, outsmarted me',
+            'I counted 8 legs... as they wrapped around me',
+            'Inked!'
+        ];
+        return $this->render('genus/show.html.twig', array(
+            'name' => $genusName,
+            'notes' => $notes
+        ));
+    }
+}
